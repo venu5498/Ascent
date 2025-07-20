33aa9e7a84af6b6da7d33b08001b583c2d8d48b3
venu5498 <venugopaltatikondac8@gmail.com>
2025-07-20 14:16:46 +0530

    authentication and authorization

---
+++ app/page.js
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+  <div className="mt-40">
+    <Button variant="destructive">Submit</Button>
+  </div>
+  );
+}
